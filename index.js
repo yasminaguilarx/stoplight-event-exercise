e5a58a8fa1ffaf2414b82d1e325feaeae692833d
yasminaguilarx <yaguilar2001@gmail.com>
2023-05-11 21:17:31 -0500

    stop-light-exercise solution

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-(function() {
-  'use strict';
-
-  // YOUR CODE HERE
-})();
+   
+    // Grab all the button elements and the three bulbs
+  let stopBtn = document.querySelector('#stopButton');
+  let slowBtn = document.querySelector('#slowButton');
+  let goBtn = document.querySelector('#goButton');
+  
+  let stopBulb = document.querySelector('#stopLight');
+  let slowBulb = document.querySelector('#slowLight');
+  let goBulb = document.querySelector('#goLight');
+  
+  const div = document.getElementsByTagName('div')[0];
+    
+  div.addEventListener('click', (event) => {
+    if(event.target.id === 'stopButton') {
+      stopBulb.classList.toggle('stop');
+      console.log(`${stopBtn.textContent} bulb ${stopLight.classList.contains('stop') ? 'on' : 'off'}`)
+    } 
+    if(event.target.id === 'slowButton') {
+      slowBulb.classList.toggle('slow');
+      console.log(`${slowBtn.textContent} bulb ${slowLight.classList.contains('slow') ? 'on' : 'off'}`)
+    } 
+    if(event.target.id === 'goButton') {
+      goBulb.classList.toggle('go');
+      console.log(`${goBtn.textContent} bulb ${goLight.classList.contains('go') ? 'on' : 'off'}`)
+    } 
+  });
